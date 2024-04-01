dca4ffa4e674c4759df7e3ca70a90242a220ef18
jungskky <38771771+jungskky@users.noreply.github.com>
2024-04-01 11:12:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+import os
 
 os.environ["OPENAI_API_KEY"] = ""
 
